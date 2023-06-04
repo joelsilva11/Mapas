a4606ecba795b2f26b037da5e535be0ada336595
joelsilva11 <ssjm1177@gmail.com>
2023-06-04 00:29:45 -0400

    filtros personalizado

---
+++ MyDropdown.py
@@ -0,0 +1,129 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Input, Output, State, callback_context
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+options = [{'label': f'Opción {i+1}', 'value': f'Valor {i+1}'} for i in range(10)]
+options2 = [{'label': f'Opc {i+1}', 'value': f'Numero {i+1}'} for i in range(10)]
+options3 = [{'label': f'Opt {i+1}', 'value': f'Num {i+1}'} for i in range(10)]
+
+def create_dropdown(id_suffix, dp_options,title_dp = 'Title'):
+    # Genera los IDs de los componentes con el sufijo proporcionado
+    input_id = f'dp-input-{id_suffix}'
+    button_id = f'dp-button-{id_suffix}'
+    checklist_id = f'checklist-{id_suffix}'
+    all_checklist_id = f'all-checklist-{id_suffix}'
+    container_id = f'checklist_container-{id_suffix}'
+
+    # Crea y devuelve el componente
+    return html.Div(
+        [
+            html.H5(
+                title_dp, 
+                style={
+                    'padding-left': '10px',
+                    'padding-top':'7px',
+                    'margin-bottom': '5px'
+                }
+            ),
+            dbc.InputGroup(
+                [
+                    dbc.Input(id=input_id, value="", readonly=True,),
+                    dbc.Button("▼", id=button_id, color="primary")
+                ],
+                style={
+                    'padding-left': '10px',
+                    'padding-right': '10px'
+                }
+            ),
+            html.Div(
+                [
+                html.Div(
+                    [   
+                        dbc.Checklist(options=["All"], value=[], id=all_checklist_id, style={'padding-left': 8}),
+                        dbc.Checklist(options=dp_options, value=[], id=checklist_id, style={'padding-left': 16}),
+                    ],
+                    id=container_id,
+                    style={
+                        'display':'none',
+                        #'backgroundColor': 'black',
+                        'backgroundColor': 'rgba(0, 0, 0, 0.92)', 
+                        'borderRadius': '5px',
+                        'overflow': 'auto',
+                        'maxHeight': '180px', 
+                        'position': 'absolute', 
+                        'z-index': '9999',
+                        'width': '100%',
+                    }
+                ),
+                ],
+                style={
+                    'position': 'relative',
+                    'padding-left': '10px',
+                    'margin-right': '20px'
+                }
+            ),
+        ],
+        style={
+            
+            'backgroundColor': '#222',
+            'borderRadius': '4px',
+            'height': '88px',
+            #'margin-bottom': '7px'
+        }
+    )
+
+# Usar la función para crear componentes
+dropdown1 = create_dropdown('1',options,'Bancos')
+dropdown2 = create_dropdown('2',options, 'Agencias')
+dropdown3 = create_dropdown('3',options, 'Otros')
+
+app.layout = html.Div([dropdown1, dropdown2,dropdown3], 
+                    style={
+                          'width': '15%',
+                          #'display': 'flex', 
+                          #'flex-direction': 'row'
+                    }
+)
+
+def create_callbacks(id_suffix):
+    # Genera los IDs de los componentes con el sufijo proporcionado
+    input_id = f'dp-input-{id_suffix}'
+    button_id = f'dp-button-{id_suffix}'
+    checklist_id = f'checklist-{id_suffix}'
+    all_checklist_id = f'all-checklist-{id_suffix}'
+    container_id = f'checklist_container-{id_suffix}'
+
+    @app.callback(
+        Output(checklist_id, "value"),
+        Output(all_checklist_id, "value"),
+        Input(checklist_id, "value"),
+        Input(all_checklist_id, "value"),
+    )
+    def sync_checklists(item_selected, all_selected):
+        ctx = dash.callback_context
+        input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+        if input_id == checklist_id:
+            all_selected = ["All"] if set(item_selected) == set([i['value'] for i in options]) else []
+        else:
+            item_selected = [i['value'] for i in options] if all_selected else []
+
+        return item_selected, all_selected
+
+    @app.callback(
+        Output(input_id, "value"),
+        [Input(checklist_id, "value")],
+    )
+    def update_input(values):
+        labels = [option["value"] for option in options if option["value"] in values]
+        return ", ".join(labels)
+
+# Usar la función para crear callbacks
+create_callbacks('1')
+create_callbacks('2')
+create_callbacks('3')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- inputgroup.py
+++ inputgroup.py
@@ -1,46 +1,57 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc, html, Input, Output, State
+from dash import dcc, html, Input, Output, State, callback_context
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 options = [{'label': f'Opción {i+1}', 'value': f'Valor {i+1}'} for i in range(10)]
 
 app.layout = html.Div(
     [
-        dbc.InputGroup(
-            [
-                dbc.Input(id="input", value=""),
-                dbc.Button("▼", id="dropdown-button", color="secondary")
-            ]
-        ),
         html.Div(
             [
-                html.Div(
-                    dcc.Checklist(options=options, value=[], id="checklist"),
-                    style={'padding-left': 8}
+                html.H5("Bancos", style={'padding-left': '10px','padding-top':'7px','margin-bottom': '5px'}),
+                dbc.InputGroup(
+                    [
+                        dbc.Input(id="input", value="", readonly=True,),
+                        dbc.Button("▼", id="kde-button", color="primary")
+                    ],
+                    style={'padding-left': '10px','padding-right': '10px'}
                 ),
+            
+        html.Div(
+            [   
+                dbc.Checklist(options=["All"], value=[], id="all-checklist"),
+                dbc.Checklist(options=options, value=[], id="checklist",style={'padding-left': 8}),
             ],
             id='checklist_container',
-            style={'display':'none'}
+            style={'display':'none','backgroundColor': 'black','margin-left': '10px','margin-right': '10px','borderRadius': '4px','overflow': 'auto','maxHeight': '180px'}
         ),
-    ]
+        ],
+            style={'backgroundColor': '#333','borderRadius': '4px','height': '88px'}
+        ),
+    ],
+    style={'width': '15%'}
 )
 
 @app.callback(
-    Output('checklist_container', 'style'),
-    [Input('dropdown-button', 'n_clicks')],
-    [State('checklist_container', 'style')]
+    Output("checklist", "value"),
+    Output("all-checklist", "value"),
+    Input("checklist", "value"),
+    Input("all-checklist", "value"),
 )
-def toggle_checklist(n, style):
-    print('Veces pulsado: ',n)
-    print('Estado display: ',style['display'])
-    if n:
-        if style['display'] == 'none':
-            return {'display': 'block'}
-        else:
-            return {'display': 'none'}
-    return style
+def sync_checklists(cities_selected, all_selected):
+    print('Valor del opciones: ',cities_selected, end='     ') 
+    print('Valor All: ', all_selected)
+
+    ctx = callback_context
+    input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if input_id == "checklist":
+        all_selected = ["All"] if set(cities_selected) == set([i['value'] for i in options]) else []
+    else:
+        cities_selected = [i['value'] for i in options] if all_selected else []
+    return cities_selected, all_selected
 
 @app.callback(
     Output("input", "value"),
@@ -48,7 +59,8 @@ def toggle_checklist(n, style):
 )
 def update_input(values):
     # Convert the values to their corresponding labels
-    labels = [option["label"] for option in options if option["value"] in values]
+    #labels = [option["label"] for option in options if option["value"] in values]
+    labels = [option["value"] for option in options if option["value"] in values]
 
     # Join all labels with commas and return the result
     return ", ".join(labels)
